8b8a6046c64cf640cb19422df7a99c50ebddfe42
vikramcm <vikramcmhr@gmail.com>
2019-04-30 16:24:16 +0530

    updated java file:

--- Jenkins1.java
+++ Jenkins1.java
@@ -1,5 +1,9 @@
 public class Jenkins1 {
 	public static void main(String[] args) {
-		System.out.println("TTD");
+		int n=10;
+		for(int i=0;i<=n;i++)
+		{
+			System.out.println("Hello World : " +i);
+		}
 	}
 }
